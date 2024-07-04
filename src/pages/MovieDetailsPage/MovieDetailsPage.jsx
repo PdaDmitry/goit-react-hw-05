bd08092a824c22f2999ea18302da14b76043625a
PdaDmitry <pda.gazcomp@gmail.com>
2024-07-04 11:04:29 +0300

    MovieDetailsPage  (Destructuring)

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -23,7 +23,6 @@ export default function MovieDetailsPage() {
 
         const result = await getMovieById(movieId, { abortController: controller }); //received movie by id
         setMovie(result);
-        // console.log('MovieDetails ', result);
       } catch (error) {
         if (error.code !== 'ERR_CANCELED') {
           console.log(error);
@@ -40,13 +39,27 @@ export default function MovieDetailsPage() {
     };
   }, [movieId]);
 
+  const {
+    backdrop_path,
+    poster_path,
+    title,
+    release_date,
+    vote_average,
+    production_countries,
+    overview,
+    genres,
+    original_language,
+  } = movie || {};
+
+  let score = Math.round(vote_average * 10);
+
   return (
     <div className={css.container}>
       {movie && (
         <div
           className={css.backgroundOverlay}
           style={{
-            backgroundImage: `url(https://image.tmdb.org/t/p/w500${movie.backdrop_path})`,
+            backgroundImage: `url(https://image.tmdb.org/t/p/w500${backdrop_path})`,
           }}
         />
       )}
@@ -58,33 +71,33 @@ export default function MovieDetailsPage() {
         <div className={css.contMovie}>
           <div>
             <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
+              src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+              alt={title}
               style={{ maxWidth: '300px', borderRadius: '4px' }}
             />
           </div>
           <ul className={css.contDataMovie}>
             <li>
               <h2>
-                {movie.title} ({movie.release_date.slice(0, 4)})
+                {title} ({release_date.slice(0, 4)})
               </h2>
-              <p>User score: {Math.round(movie.vote_average * 10)}%</p>
+              <p>User score: {score}%</p>
             </li>
             <li>
               <h3>Country</h3>
-              <p>{movie.production_countries.map(item => item.name).join(', ')}</p>
+              <p>{production_countries.map(item => item.name).join(', ')}</p>
             </li>
             <li>
               <h3>Overview: </h3>
-              <p>{movie.overview}</p>
+              <p>{overview}</p>
             </li>
             <li>
               <h3>Genres</h3>
-              <p>{movie.genres.map(item => item.name).join(', ')}</p>
+              <p>{genres.map(item => item.name).join(', ')}</p>
             </li>
             <li>
               <h4>Original Language:</h4>
-              <p> {movie.original_language}</p>
+              <p>{original_language}</p>
             </li>
           </ul>
         </div>
